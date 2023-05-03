a34bb4c7b2e692fadb22d328e32885621cbf5844
oxc <524112857@qq.com>
2023-05-03 20:01:53 +0800

    feat

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import { fabric } from 'fabric'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
 import ZoomController from './ZoomController'
 
 const url = "https://img.ixintu.com/download/jpg/201910/f4d4228a35aa7fd350c058863a4cb4c2.jpg"
 
 const App = () => {
+  const zoomController = useRef(null)
   // canvas实例
   const [canvas, setCanvas] = useState(null)
+  const [id, setId] = useState(1)
   // img实例
   const [img, setImg] = useState(null)
+  // 比例
+  const [zoom, setZoom] = useState(0)
   // 中心点
   // const [centerX, setCenterX] = useState(0)
   // const [centerY, setCenterY] = useState(0)
 
   // 初始化画布
   const initCanvas = () => {
-    const c = new fabric.Canvas('canvas')
+    const c = new fabric.Canvas(`canvas_${id}`)
     fabric.Image.fromURL(url, img => {
       c.selection = false
       img.set('hasBorders', false)
@@ -26,6 +31,7 @@ const App = () => {
       c.centerObject(img)
       setImg(img)
       setCanvas(c)
+      setZoom(c.getZoom())
       initScrollEvent(c, img)
       initDragEvent(c, img)
     })
@@ -35,60 +41,78 @@ const App = () => {
   const initScrollEvent = (c, img) => {
     c.on('mouse:wheel', (opt) => {
       const delta = opt.e.deltaY
-      let zoom = c.getZoom()
+      let z = c.getZoom()
       if (delta > 0) {
-        zoom = zoom - 0.05
+        z = z - 0.05
       } else {
-        zoom = zoom + 0.05
+        z = z + 0.05
       }
-      if (zoom > 2) zoom = 2
-      if (zoom < 0.2) zoom = 0.2
+      if (z > 2) z = 2
+      if (z < 0.2) z = 0.2
+      // c.setZoom(1)
+      // const vpw = c.width / z
+      // const vph = c.height / z
+      // const x = (img.left - vpw / 2)
+      // const y = (img.top - vph / 2)
+      // c.absolutePan({x:x, y:y})
+      // c.setZoom(z)
       const center = img.getCenterPoint()
       c.zoomToPoint({
         x: center.x,
         y: center.y
-      }, zoom)
+      }, z)
     })
   }
 
   // 初始化拖拽
   const initDragEvent = (c, img) => {
-    // // 按下鼠标
-    // c.on('mouse:down', (opt) => {
-    //   setViewPort(c.viewportTransform)
-    // })
-    // // 拖拽
-    // c.on('mouse:move', (opt) => {
-    //   setViewPort(c.viewportTransform)
-    // })
-    // // 松开鼠标
-    // c.on('mouse:up', (opt) => {
-    //   setViewPort(c.viewportTransform)
-    // })
+    // 按下鼠标
+    c.on('mouse:down', (opt) => {
+      const center = img.getCenterPoint()
+    })
+    // 拖拽
+    c.on('mouse:move', (opt) => {
+      
+    })
+    // 松开鼠标
+    c.on('mouse:up', (opt) => {
+      const center = img.getCenterPoint()
+      // console.log(img)
+    })
   }
 
   // 初始化
   useEffect(() => {
-    initCanvas()
+    setId(uuidv4())
   }, [])
+
+  useEffect(() => {
+    if (id !== 1) {
+      initCanvas()
+    }
+  }, [id])
 
 
   return (
     <div
       id="canvasWrapper"
-      style={{ display: 'flex' }}
     >
       <canvas
-        id="canvas"
+        id={`canvas_${id}`}
         width={1500}
         height={1000}></canvas>
-        {
-          canvas && img &&
-          <ZoomController
-            canvas={canvas}
-            img={img}
-          />
-        }
+        <div>
+          {
+            canvas && img &&
+            <ZoomController
+              canvas={canvas}
+              img={img}
+              zoom={zoom}
+              setZoom={setZoom}
+            />
+          }
+        </div>
+
     </div>
   )
   
--- src/ZoomController.js
+++ src/ZoomController.js
@@ -1,15 +1,46 @@
 import './index.css'
-import { useState, useEffect } from 'react'
 
-const ZoomController = ({ canvas, img }) => {
-    const [zoom, setZoom] = useState(0)
+const zoomControllerList = [
+    {
+      zoomText: '20%',
+      zoomNum: 0.2,
+    },
+    {
+      zoomText: '50%',
+      zoomNum: 0.5,
+    },
+    {
+      zoomText: '75%',
+      zoomNum: 0.75,
+    },
+    {
+      zoomText: '100%',
+      zoomNum: 1,
+    },
+    {
+      zoomText: '150%',
+      zoomNum: 1.5,
+    },
+    {
+      zoomText: '200%',
+      zoomNum: 2,
+    },
+    {
+      zoomText: '原始尺寸',
+      zoomNum: 1,
+      shortcutText: 'Ctrl+1',
+    },
+    {
+      zoomText: '适应窗口',
+      zoomNum: 5,
+      shortcutText: 'Shift+1',
+    },
+  ]
 
-    useEffect(() => {
-        setZoom(canvas.getZoom())
-    }, [])
+const ZoomController = ({ canvas, img, zoom, setZoom }) => {
 
     const clickBtn = (type) => {
-        let z = zoom
+        let z = canvas.getZoom()
         if (type === 'add') {
             z = z + 0.05
         } else if (type === 'minus') {
@@ -24,12 +55,32 @@ const ZoomController = ({ canvas, img }) => {
         }, z)
     }
 
+    const click = (zoom) => {
+        canvas.centerObject(img)
+        const center = img.getCenterPoint()
+        canvas.zoomToPoint({
+            x: center.x,
+            y: center.y
+        }, zoom)
+    }
+
 
     return (
         <div className="aaaa">
             <button onClick={() => clickBtn('add')}>+</button>
             <div>{(zoom * 100).toFixed(0)}</div>
             <button onClick={() => clickBtn('minus')}>-</button>
+            <div>
+                {
+                    zoomControllerList.map((item, i) => {
+                        return (
+                            <button onClick={() => click(item.zoomNum)} key={i}>
+                                {item.zoomText}
+                            </button>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
